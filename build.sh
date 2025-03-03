7b8a7f65f398628bc5110cccfbbef21c4fd4f176
Nick Silvia <nick@Nicks-iMac.local>
2025-03-02 19:22:52 -0500

    Fix Playwright browser installation issue

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pip install --upgrade pip
 pip install -r requirements.txt
-python -m playwright install --with-deps
+python -m playwright install --with-deps --force
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-source /opt/render/project/src/.venv/bin/activate  # Ensure we use the right Python environment
+source /opt/render/project/src/.venv/bin/activate
 exec gunicorn -b 0.0.0.0:8080 server:app
