790592093dbe498eeb7221dbf14d7c03902fb354
Nick Silvia <nick@Nicks-iMac.local>
2025-03-02 19:29:55 -0500

    Force Playwright to install locally in Render

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/.cache/ms-playwright
 pip install --upgrade pip
 pip install -r requirements.txt
-PLAYWRIGHT_BROWSERS_PATH=0 python -m playwright install --with-deps
+python -m playwright install --with-deps
--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-export PLAYWRIGHT_BROWSERS_PATH=0  # Ensures Playwright uses local cache
+export PLAYWRIGHT_BROWSERS_PATH=/opt/render/project/.cache/ms-playwright
 source /opt/render/project/src/.venv/bin/activate
 exec gunicorn -b 0.0.0.0:8080 server:app
